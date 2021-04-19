66f29129de774d5024e32d616fcb08c9e46175dd
Alvaro-MC <alvaro.chico@grupomcperu.com>
2021-04-19 09:57:35 -0500

    aaa
    aa

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Surprise</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!-- FONTAWESOME -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
+
+    <link rel="Icon" href="img/favicon.svg" type=”image/x-icon” />
+
+    <!-- BOOTSTRAP CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- BOOTSTRAP JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
+
+</head>
+
+<body>
+    <div class="container-fluid container-vh">
+        <div class="row h-100">
+            <div class="col-5 top-0 align-self-center">
+                <img class="img-fluid" src="img/personaje-mantenimiento.jpeg" alt="">
+            </div>
+            <div class="col-7 align-self-center text-center">
+                <p class="fs-1">Estamos en mantenimiento</p>
+                <p class="f-4">Visitanos en nuestras redes</p>
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <a class="fs-1 me-4" href="https://www.facebook.com/Surprise.itis/"><i class="fab fa-facebook-f"></i></a>
+                            <a class="fs-1" href="https://instagram.com/surprise.itis?igshid=tqwc8mozqqrp"><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
